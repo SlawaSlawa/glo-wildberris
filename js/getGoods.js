277ab6f25a0ab91a37580dc98cffa4e9f7f9f961
Slawa <slawapocht4@yandex.ru>
2021-11-21 23:21:55 +0400

    day three

--- js/getGoods.js
+++ js/getGoods.js
@@ -1,29 +1,65 @@
 const getGoods = () => {
     const URL = 'https://glo-wildberris-default-rtdb.firebaseio.com/db.json'
     const links = document.querySelectorAll('.navigation-link')
+    // const moreBtn = document.querySelector('.more')
 
-    const getData = () => {
+    const renderGoods = (goods) => {
+        const goodsContainer = document.querySelector('.long-goods-list')
+        goodsContainer.innerHTML = ''
+
+        goods.forEach(good => {
+            const { id, label, img, name, price, description } = good
+            const goodBlock = document.createElement('div')
+            goodBlock.classList.add('col-lg-3', 'col-sm-6')
+
+            goodBlock.innerHTML = `
+            <div class="goods-card">
+                <span class="label ${label ? null : 'd-none'}">${label}</span>
+                <img src="db/${img}" alt="image: ${name}" class="goods-image">
+                <h3 class="goods-title">${name}</h3>
+                <p class="goods-description">${description}</p>
+                <button class="button goods-card-btn add-to-cart" data-id="${id}">
+                    <span class="button-price">$${price}</span>
+                </button>
+            </div>
+            `
+
+            goodsContainer.append(goodBlock)
+        })
+    }
+
+    const getData = (value, category) => {
         fetch(URL)
             .then(response => response.json())
             .then(data => {
-                localStorage.setItem('goods', JSON.stringify(data))
+                const array = category ? data.filter(item => item[category] === value) : data
+                localStorage.setItem('goods', JSON.stringify(array))
 
+                if (window.location.pathname !== '/goods.html') {
+                    window.location.href = '/goods.html'
+                } else {
+                    renderGoods(array)
+                }
             })
     }
 
     links.forEach(link => {
         link.addEventListener('click', (evt) => {
             evt.preventDefault();
-            const target = evt.target
-            getData()
+            const linkValue = link.textContent
+            const category = link.dataset.field
+            getData(linkValue, category)
         })
     })
 
-    localStorage.setItem('goods', JSON.stringify({ name: 'all' }))
-    const goods = JSON.parse(localStorage.getItem('goods'))
-    console.log(localStorage)
-    localStorage.removeItem('goods')
-    console.log(localStorage)
+    // moreBtn.addEventListener('click', (evt) => {
+    //     evt.preventDefault()
+    //     getData()
+    // })
+
+    if (localStorage.getItem('goods') && window.location.pathname === '/goods.html') {
+        renderGoods(JSON.parse(localStorage.getItem('goods')))
+    }
 }
 
 getGoods();
--- js/search.js
+++ js/search.js
@@ -1,15 +1,64 @@
 const search = () => {
+    const URL = 'https://glo-wildberris-default-rtdb.firebaseio.com/db.json'
+
     const input = document.querySelector('.search-block > input')
     const searchBtn = document.querySelector('.search-block > button')
+
+    const renderGoods = (goods) => {
+        const goodsContainer = document.querySelector('.long-goods-list')
+        goodsContainer.innerHTML = ''
+
+        goods.forEach(good => {
+            const { id, label, img, name, price, description } = good
+            const goodBlock = document.createElement('div')
+            goodBlock.classList.add('col-lg-3', 'col-sm-6')
+
+            goodBlock.innerHTML = `
+            <div class="goods-card">
+                <span class="label ${label ? null : 'd-none'}">${label}</span>
+                <img src="db/${img}" alt="image: ${name}" class="goods-image">
+                <h3 class="goods-title">${name}</h3>
+                <p class="goods-description">${description}</p>
+                <button class="button goods-card-btn add-to-cart" data-id="${id}">
+                    <span class="button-price">$${price}</span>
+                </button>
+            </div>
+            `
+
+            goodsContainer.append(goodBlock)
+        })
+    }
+
+    const getData = (value) => {
+        fetch(URL)
+            .then(response => response.json())
+            .then(data => {
+                const array = data.filter(good => {
+                    return good.name.toLowerCase().includes(value.toLowerCase())
+                })
+
+                localStorage.setItem('goods', JSON.stringify(array))
+
+                if (window.location.pathname !== '/goods.html') {
+                    window.location.href = '/goods.html'
+                } else {
+                    renderGoods(array)
+                }
+            })
+    }
 
     // input.addEventListener('input', (evt) => {
     //     const target = evt.target
     //     console.log('value: ', target.value);
     // })
 
-    searchBtn.addEventListener('click', () => {
-        console.log(input.value)
-    })
+    try {
+        searchBtn.addEventListener('click', () => {
+            getData(input.value)
+        })
+    } catch (error) {
+        console.error(error.message)
+    }
 
 }
 
